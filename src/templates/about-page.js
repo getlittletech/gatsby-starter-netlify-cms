1a19cd233fabacf30312aba0b4489ec2f5bc3dc5
Olegas Kosuchinas <oleg@Olegass-MacBook-Pro.local>
2022-09-29 09:16:31 +0200

    test

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -10,6 +10,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 
   return (
     <section className="section section--gradient">
+      <div>This should always be good</div>
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
