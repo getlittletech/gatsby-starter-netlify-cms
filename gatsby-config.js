85a770af9cb2e5496ac62cc73d64a3d3956f6f97
Olegas <getlittletech@gmail.com>
2022-10-05 21:39:09 +0200

    update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "SoftLane - web and apps with React",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "We are an IT consultancy company focusing on building web and app solutions with React, React Native, Node.js, REST and/or GraphQl. Building it with agile methodology and test driven approach.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
