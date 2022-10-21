11edb533b7c69ae089ce867c779424e213b5bb4e
Olegas <getlittletech@gmail.com>
2022-10-21 21:30:02 +0200

    logo image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.png";
+import logo from "../img/brand/logo.png";
 
 const Navbar = class extends React.Component {
   constructor(props) {
